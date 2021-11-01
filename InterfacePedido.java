c3626583c000f265799edb4a0d66e24cced63f31
Andevson <andersonteodorodejesus@hotmail.com>
2021-11-01 17:33:45 -0300

    format tbl_produtos and add update table code from InterfaceNovoPedido.java

--- InterfacePedido.java
+++ InterfacePedido.java
@@ -2,36 +2,52 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.table.AbstractTableModel;
 
 public class InterfacePedido extends JFrame{
-    private final JTextArea txt_entrada = new JTextArea("Insira um produto.");
+    private final JLabel lbl_entrada = new JLabel("Entre com código de barras:");
+    private final JTextArea txt_entrada = new JTextArea("");
     private final JLabel lbl_qty_restante = new JLabel("Finalizar Pedido");
-    private final JTable tbl_produtos = new JTable();
+    private Object[][] dados = {{"", ""}};
+    private String[] header = {"ID", "Código"};
+    private final JTable tbl_produtos = new JTable(dados, header);
     private Pedido p;
+    void atualizaTabela(Pedido pedido){
+        JTable nova_tbl_produtos = pedido.getTabelaProdutos();
+        tbl_produtos.setModel(nova_tbl_produtos.getModel());
+        ((AbstractTableModel) tbl_produtos.getModel()).fireTableDataChanged();
+    }
     InterfacePedido(Pedido pedido){
-        super("Novo pedido");
+        super("Pedido");
         JPanel painel = new JPanel();
         JPanel topo = new JPanel();
         JPanel centro = new JPanel();
         painel.setLayout(new BorderLayout());
         topo.setLayout(new FlowLayout());
-        centro.setLayout(new FlowLayout());
+        centro.setLayout(new BorderLayout());
         add(painel);
         painel.add(topo, BorderLayout.NORTH);
         painel.add(centro, BorderLayout.CENTER);
+        topo.add(lbl_entrada);
         topo.add(txt_entrada);
         topo.add(lbl_qty_restante);
-        centro.add(tbl_produtos);
-        setBounds(50, 50, 400, 550);
+        centro.add(tbl_produtos.getTableHeader(), BorderLayout.NORTH);
+        centro.add(tbl_produtos, BorderLayout.CENTER);
+        setBounds(450, 50, 400, 550);
+        txt_entrada.setMinimumSize(new Dimension(100, 20));
+        txt_entrada.setPreferredSize(new Dimension(100, 20));
+        txt_entrada.setMaximumSize(new Dimension(100, 20));
         this.p = pedido;
-        lbl_qty_restante.setText("Quantidade: " + p.getQuantidade());
+        atualizaTabela(p);
+        lbl_qty_restante.setText("Restante: " + p.getQuantidade());
         txt_entrada.getDocument().addDocumentListener(new DocumentListener(){
             @Override
             public void insertUpdate(DocumentEvent e) {
                 String entrada = txt_entrada.getText();
                 if(entrada.length() >= 14){
                     p = App.saida(pedido, entrada);
-                    lbl_qty_restante.setText("Quantidade: " + p.getQuantidade());
+                    atualizaTabela(p);
+                    lbl_qty_restante.setText("Restante: " + p.getQuantidade());
                     SwingUtilities.invokeLater(new Runnable(){
                         @Override
                         public void run() {

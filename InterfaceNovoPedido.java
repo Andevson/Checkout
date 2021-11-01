d266406758b5723b8dc12563997892a1f90cd7c9
Andevson <andersonteodorodejesus@hotmail.com>
2021-11-01 16:24:10 -0300

    format tbl_produtos and add update table code from App.java

--- InterfaceNovoPedido.java
+++ InterfaceNovoPedido.java
@@ -2,12 +2,15 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
 
 public class InterfaceNovoPedido extends JFrame{
     private final JTextArea txt_entrada = new JTextArea("");
     private final JButton btn_obter = new JButton("Obter produtos");
     private final JButton btn_finalizar = new JButton("Finalizar Pedido");
-    private final JTable tbl_produtos = new JTable();
+    private Object[][] dados = {{"", ""}};
+    private String[] header = {"ID", "Código"};
+    private JTable tbl_produtos = new JTable(dados, header);
     private Pedido p;
     InterfaceNovoPedido(Pedido pedido){
         super("Novo pedido");
@@ -16,14 +19,15 @@ public class InterfaceNovoPedido extends JFrame{
         JPanel centro = new JPanel();
         painel.setLayout(new BorderLayout());
         topo.setLayout(new FlowLayout());
-        centro.setLayout(new FlowLayout());
+        centro.setLayout(new BorderLayout());
         add(painel);
         painel.add(topo, BorderLayout.NORTH);
         painel.add(centro, BorderLayout.CENTER);
         topo.add(txt_entrada);
         topo.add(btn_obter);
         topo.add(btn_finalizar);
-        centro.add(tbl_produtos);
+        centro.add(tbl_produtos.getTableHeader(), BorderLayout.NORTH);
+        centro.add(tbl_produtos, BorderLayout.CENTER);
         setBounds(450, 50, 400, 550);
         txt_entrada.setMinimumSize(new Dimension(100, 20));
         txt_entrada.setPreferredSize(new Dimension(100, 20));
@@ -33,6 +37,9 @@ public class InterfaceNovoPedido extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e){
                 p = App.inserirProdutos(p, txt_entrada.getText());
+                JTable nova_tbl_produtos = p.getTabelaProdutos();
+                tbl_produtos.setModel(nova_tbl_produtos.getModel());
+                ((AbstractTableModel) tbl_produtos.getModel()).fireTableDataChanged();
                 txt_entrada.setText("");
             }
         });

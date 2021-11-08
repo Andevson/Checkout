baf7eeba0782a45a455bf148453f77d07a00ee36
Andevson <andersonteodorodejesus@hotmail.com>
2021-11-07 21:06:37 -0300

    add Produto.java

--- Produto.java
+++ Produto.java
@@ -1,14 +1,19 @@
 public class Produto{
     private String id;
     private String codigo;
-    Produto(String id, String codigo){
+    private int fator_de_saida;
+    Produto(String id, String codigo, int fator_de_saida){
         this.id = id;
         this.codigo = codigo;
+        this.fator_de_saida = fator_de_saida;
     }
     public String getId(){
         return id;
     }
     public String getCodigo(){
         return codigo;
+    }
+    public int getFator(){
+        return fator_de_saida;
     }
 }

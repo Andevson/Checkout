713a7f521103156bdc4998291ec6b369ab56363e
Andevson <andersonteodorodejesus@hotmail.com>
2021-11-07 21:10:59 -0300

    add 'produtos_quantidade' to store each product's quantity from Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -2,9 +2,11 @@ import javax.swing.JTable;
 
 public class Pedido {
     private Produto[] produtos;
+    private int[] produtos_quantidade;
     private int quantidade;
     Pedido(){
         produtos = new Produto[100];
+        produtos_quantidade = new int[100];
         quantidade = 0;
     }
     Pedido(Produto[] produtos, int quantidade){
@@ -17,17 +19,23 @@ public class Pedido {
     public int getQuantidade(){
         return quantidade;
     }
-    public void acrescentarProduto(Produto produto){
+    private int[] getProdutosQuantidade(){
+        return produtos_quantidade;
+    }
+    public void acrescentarProduto(Produto produto, int produto_quantidade){
         quantidade += 1;
         produtos[quantidade] = produto;
+        produtos_quantidade[quantidade] = produto_quantidade;
     }
     public void saidaProduto(String codigo){
         Produto[] produtos_atual = new Produto[100];
+        int[] produtos_quantidade_atual = new int[100];
         int n = 1;
         int i = 1;
         while(n <= quantidade){
             if(!codigo.equals(produtos[n].getCodigo())){
                 produtos_atual[i] = produtos[n];
+                produtos_quantidade_atual[i] = produtos_quantidade[n];
                 i++;
             }else{
                 codigo = "vazio";
@@ -35,14 +43,16 @@ public class Pedido {
             n++;
         }
         produtos = produtos_atual;
+        produtos_quantidade = produtos_quantidade_atual;
         quantidade = i - 1;
     }
     public JTable getTabelaProdutos(){
-            Object[][] dados = new String[quantidade][2];
-            String[] cabecalho = {"ID", "Código de barras"};
+            Object[][] dados = new String[quantidade][3];
+            String[] cabecalho = {"ID", "Código de barras", "quantidade"};
             for(int n = 0; n < quantidade; n++){
                 dados[n][0] = produtos[n + 1].getId();
                 dados[n][1] = produtos[n + 1].getCodigo();
+                dados[n][2] = String.valueOf(getProdutosQuantidade()[n + 1]);
             }
             return new JTable(dados, cabecalho);
     }

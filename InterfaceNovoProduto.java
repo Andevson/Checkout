64644d5e911679353243b5c9cc0f8ad2468cd039
Andevson <andersonteodorodejesus@hotmail.com>
2021-11-09 20:17:46 -0300

    add 'txt_fator' and 'lbl_fator' to receive 'fator' and insert into 'App.cadastrarProduto()' from InterfaceNovoProduto.java

--- InterfaceNovoProduto.java
+++ InterfaceNovoProduto.java
@@ -8,6 +8,8 @@ public class InterfaceNovoProduto extends JFrame{
     private final JTextField txt_id = new JTextField("");
     private final JLabel lbl_codigo = new JLabel("Código:");
     private final JTextField txt_codigo = new JTextField("");
+    private final JLabel lbl_fator = new JLabel("Fator:");
+    private final JTextField txt_fator = new JTextField("1");
     private final JButton btn_cadastrar = new JButton("Cadastrar produto");
     InterfaceNovoProduto(String id){
         super("Cadastro de novo produto");
@@ -24,17 +26,22 @@ public class InterfaceNovoProduto extends JFrame{
         centro.add(txt_id);
         centro.add(lbl_codigo);
         centro.add(txt_codigo);
+        centro.add(lbl_fator);
+        centro.add(txt_fator);
         sul.add(btn_cadastrar);
         setBounds(450, 50, 400, 120);
         setResizable(false);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         App.setIcone(this);
-        txt_id.setMinimumSize(new Dimension(100, 25));
-        txt_id.setPreferredSize(new Dimension(100, 25));
-        txt_id.setMaximumSize(new Dimension(100, 25));
+        txt_id.setMinimumSize(new Dimension(75, 25));
+        txt_id.setPreferredSize(new Dimension(75, 25));
+        txt_id.setMaximumSize(new Dimension(75, 25));
         txt_codigo.setMinimumSize(new Dimension(100, 25));
         txt_codigo.setPreferredSize(new Dimension(100, 25));
         txt_codigo.setMaximumSize(new Dimension(100, 25));
+        txt_fator.setMinimumSize(new Dimension(50, 25));
+        txt_fator.setPreferredSize(new Dimension(50, 25));
+        txt_fator.setMaximumSize(new Dimension(50, 25));
         txt_id.setText(id);
         addWindowFocusListener(new WindowAdapter() {
             public void windowGainedFocus(WindowEvent e) {
@@ -48,7 +55,7 @@ public class InterfaceNovoProduto extends JFrame{
         btn_cadastrar.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(App.cadastrarProduto(txt_id.getText(), txt_codigo.getText())){
+                if(App.cadastrarProduto(txt_id.getText(), txt_codigo.getText(), txt_fator.getText())){
                     dispose();
                 }
             }

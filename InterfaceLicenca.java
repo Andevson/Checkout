4fcdf88bc70713201a68b911c2b08f0f2b3655ea
Andevson <andersonteodorodejesus@hotmail.com>
2021-11-14 20:23:00 -0300

    add 'txt_info' and 'licenca' and 'info' to encapsulate 'txt_licenca' and 'txt_info' from InterfaceLicenca.java

--- InterfaceLicenca.java
+++ InterfaceLicenca.java
@@ -6,24 +6,36 @@ public class InterfaceLicenca extends JDialog{
     JPanel painel = new JPanel();
     JPanel centro = new JPanel();
     JPanel sul = new JPanel();
+    JPanel licenca = new JPanel();
+    JPanel info = new JPanel();
+    JScrollPane centro_scroll = new JScrollPane(licenca);
     private final JLabel txt_licenca = new JLabel("");
+    private final JLabel txt_info = new JLabel("<html><head></head><body><span>github.com/andevson/checkout</span></body></html>");
     private final JButton btn_ok = new JButton("Ok");
     InterfaceLicenca(){
         setVisible(true);
         setTitle("Licença");
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         setModal(true);
-        setResizable(false);
+        setResizable(true);
         App.setIcone(this);
-        setBounds(500, 100, 250, 250);
+        setBounds(50, 50, 400, 450);
         painel.setLayout(new BorderLayout());
         centro.setLayout(new FlowLayout());
+        centro.setPreferredSize(new Dimension(385, 430));
+        licenca.setPreferredSize(new Dimension(365, 450));
+        txt_licenca.setPreferredSize(new Dimension(365, 450));
+        centro_scroll.setPreferredSize(new Dimension(385, 320));
+        info.setPreferredSize(new Dimension(375, 50));
         sul.setLayout(new FlowLayout());
         add(painel);
         painel.add(centro, BorderLayout.CENTER);
         painel.add(sul, BorderLayout.SOUTH);
         txt_licenca.setText(App.getLicenca());
-        centro.add(txt_licenca);
+        licenca.add(txt_licenca);
+        info.add(txt_info);
+        centro.add(centro_scroll);
+        centro.add(info);
         sul.add(btn_ok);
         btn_ok.addActionListener(new ActionListener(){
             @Override

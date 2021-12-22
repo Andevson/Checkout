09f94964036365879af24912581b091d291343df
Andevson <andersonteodorodejesus@hotmail.com>
2021-12-22 10:06:57 -0300

    change 'codigo_cliente' to 'codigo_barras' from PedidoORM.java

--- PedidoORM.java
+++ PedidoORM.java
@@ -1,11 +1,11 @@
 public class PedidoORM {
     private String numero_pedido;
     private String quantidade;
-    private String codigo_cliente;
-    PedidoORM(String numero_pedido, String quantidade, String codigo_cliente){
+    private String codigo_barras;
+    PedidoORM(String numero_pedido, String quantidade, String codigo_barras){
         this.numero_pedido = numero_pedido;
         this.quantidade = quantidade;
-        this.codigo_cliente = codigo_cliente;
+        this.codigo_barras = codigo_barras;
     }
     public String getNumeroPedido(){
         return numero_pedido;
@@ -13,7 +13,7 @@ public class PedidoORM {
     public String getQuantidade(){
         return quantidade;
     }
-    public String getCodigoCliente(){
-        return codigo_cliente;
+    public String getCodigoBarras(){
+        return codigo_barras;
     }
 }
